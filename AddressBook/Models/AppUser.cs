94a36dcc33cce1929d456cc0b80bc98b23ec451b
jedmorris <jemorris110@gmail.com>
2022-01-28 09:37:05 -0800

    created AppUser model and added first, last, and full name parameters

--- AddressBook/Models/AppUser.cs
+++ AddressBook/Models/AppUser.cs
@@ -1,6 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace AddressBook.Models;
 
-public class AppUser
+public class AppUser : IdentityUser
 {
-    
+   public string FirstName { get; set; }
+   public string LastName { get; set; }
+   
+   public string FullName { get; set; }
 }
