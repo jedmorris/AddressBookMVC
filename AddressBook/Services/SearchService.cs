587fae4ab48fbfec0ab2eb71946a759dbbf0586c
jedmorris <jemorris110@gmail.com>
2022-02-03 14:22:39 -0800

    corrected ; error

--- AddressBook/Services/SearchService.cs
+++ AddressBook/Services/SearchService.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Contracts;
 using AddressBook.Data;
 using Microsoft.EntityFrameworkCore;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping;
@@ -6,19 +7,19 @@ namespace AddressBook.Services;
 
 public class SearchService
 {
-//    private readonly ApplicationDbContext _context;
-//
-//    public SearchService(ApplicationDbContext context)
-//    {
-//    }
-//
-//    public IEnumerable<Contract> SearchContacts(string searchString, string userId)
-//    {
-//       var result = _context.Contacts.Include(c => c.Categories).Where(c => c.UserId == userId).AsQueryable();
-//
-//       if (searchString != null)
-//       {
-//          result = result.Where(c => c.Address1!.ToLower().Contains(searchString.ToLower()))
-//       }
-//    }
+    // private readonly ApplicationDbContext _context;
+    //
+    // public SearchService(ApplicationDbContext context)
+    // {
+    // }
+    //
+    // public IEnumerable<Contract> SearchContacts(string searchString, string userId)
+    // {
+    //    var result = _context.Contacts.Include(c => c.Categories).Where(c => c.UserId == userId).AsQueryable();
+    //
+    //    if (searchString != null)
+    //    {
+    //        result = result.Where(c => c.Address1!.ToLower().Contains(searchString.ToLower()));
+    //    }
+    // }
 }
