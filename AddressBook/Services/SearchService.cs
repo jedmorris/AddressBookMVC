a0f011f4d96bba9545a6c528a0901624b90d19ba
jedmorris <jemorris110@gmail.com>
2022-02-03 11:16:00 -0800

    built 'SearchService' Service and commented code until Category(s) are completed

--- AddressBook/Services/SearchService.cs
+++ AddressBook/Services/SearchService.cs
@@ -1,6 +1,24 @@
+using AddressBook.Data;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Storage.Internal.Mapping;
+
 namespace AddressBook.Services;
 
 public class SearchService
 {
-    
+//    private readonly ApplicationDbContext _context;
+//
+//    public SearchService(ApplicationDbContext context)
+//    {
+//    }
+//
+//    public IEnumerable<Contract> SearchContacts(string searchString, string userId)
+//    {
+//       var result = _context.Contacts.Include(c => c.Categories).Where(c => c.UserId == userId).AsQueryable();
+//
+//       if (searchString != null)
+//       {
+//          result = result.Where(c => c.Address1!.ToLower().Contains(searchString.ToLower()))
+//       }
+//    }
 }
