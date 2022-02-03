2c4fbe7e61ec46b0754db0cdd7a10985904c9d7d
jedmorris <jemorris110@gmail.com>
2022-02-03 13:00:53 -0800

    successfully updated db in ef and confirmed it through psql ui

---
+++ AddressBook/Data/Migrations/20220203205251_Update_Init.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AddressBook.Data.Migrations
+{
+    public partial class Update_Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Categories_CategoryId",
+                table: "Contacts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Contacts_ContactId",
+                table: "Contacts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contacts_CategoryId",
+                table: "Contacts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contacts_ContactId",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "ContactId",
+                table: "Contacts");
+
+            migrationBuilder.CreateTable(
+                name: "CategoryContact",
+                columns: table => new
+                {
+                    CategoriesId = table.Column<int>(type: "integer", nullable: false),
+                    ContactsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryContact", x => new { x.CategoriesId, x.ContactsId });
+                    table.ForeignKey(
+                        name: "FK_CategoryContact_Categories_CategoriesId",
+                        column: x => x.CategoriesId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoryContact_Contacts_ContactsId",
+                        column: x => x.ContactsId,
+                        principalTable: "Contacts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryContact_ContactsId",
+                table: "CategoryContact",
+                column: "ContactsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryContact");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Contacts",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ContactId",
+                table: "Contacts",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_CategoryId",
+                table: "Contacts",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_ContactId",
+                table: "Contacts",
+                column: "ContactId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_Categories_CategoryId",
+                table: "Contacts",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_Contacts_ContactId",
+                table: "Contacts",
+                column: "ContactId",
+                principalTable: "Contacts",
+                principalColumn: "Id");
+        }
+    }
+}
